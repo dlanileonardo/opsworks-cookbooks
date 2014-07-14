f11d0bf863af378bd3cf47616a19ee12f925dfff
Dlani Mendes <dlanileonardo@gmail.com>
2014-07-14 19:54:36 -0300

    Swap

---
+++ create_swap/metadata.rb
@@ -0,0 +1,7 @@
+name        "create_swap"
+description "Create Swap"
+maintainer  "Dlani"
+license     "Apache 2.0"
+version     "0.0.1"
+
+depends 'swap'
---
+++ create_swap/recipes/default.rb
@@ -0,0 +1,3 @@
+swap_file '/mnt/swap' do
+  size 2048 # MBs
+end
