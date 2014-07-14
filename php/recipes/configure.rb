bbf450485692551a7d844772fee4203be33c6de1
Dlani Mendes <dlanileonardo@gmail.com>
2014-07-14 06:56:14 -0300

    Configure

--- php/recipes/configure.rb
+++ php/recipes/configure.rb
@@ -5,6 +5,24 @@ node[:deploy].each do |application, deploy|
   end
 
   # write out opsworks.php
+  template "#{deploy[:deploy_to]}/shared/config/opsworks.php" do
+    cookbook 'php'
+    source 'opsworks.php.erb'
+    mode '0660'
+    owner deploy[:user]
+    group deploy[:group]
+    variables(
+      :database => deploy[:database],
+      :memcached => deploy[:memcached],
+      :layers => node[:opsworks][:layers],
+      :stack_name => node[:opsworks][:stack][:name]
+    )
+    only_if do
+      File.exists?("#{deploy[:deploy_to]}/shared/config")
+    end
+  end
+
+  # write out opsworks.php
   template "#{deploy[:deploy_to]}/shared/config/settings.inc.php" do
     cookbook 'php'
     source 'settings.inc.php.erb'
@@ -13,9 +31,6 @@ node[:deploy].each do |application, deploy|
     group deploy[:group]
     variables(
       :database => deploy[:database],
-      # :memcached => deploy[:memcached],
-      # :layers => node[:opsworks][:layers],
-      # :stack_name => node[:opsworks][:stack][:name]
     )
     only_if do
       File.exists?("#{deploy[:deploy_to]}/shared/config")
