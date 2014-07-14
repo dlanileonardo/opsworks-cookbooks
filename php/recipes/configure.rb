944eed328547e8ad75ad1d08928b16eab362d2b1
Dlani Mendes <dlanileonardo@gmail.com>
2014-07-14 05:44:21 -0300

    Config OpsWorks

--- php/recipes/configure.rb
+++ php/recipes/configure.rb
@@ -5,17 +5,17 @@ node[:deploy].each do |application, deploy|
   end
 
   # write out opsworks.php
-  template "#{deploy[:deploy_to]}/shared/config/opsworks.php" do
+  template "#{deploy[:deploy_to]}/shared/config/settings.inc.php" do
     cookbook 'php'
-    source 'opsworks.php.erb'
+    source 'settings.inc.php.erb'
     mode '0660'
     owner deploy[:user]
     group deploy[:group]
     variables(
       :database => deploy[:database],
-      :memcached => deploy[:memcached],
-      :layers => node[:opsworks][:layers],
-      :stack_name => node[:opsworks][:stack][:name]
+      # :memcached => deploy[:memcached],
+      # :layers => node[:opsworks][:layers],
+      # :stack_name => node[:opsworks][:stack][:name]
     )
     only_if do
       File.exists?("#{deploy[:deploy_to]}/shared/config")
