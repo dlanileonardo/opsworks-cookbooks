472f7ed8fefb9b02c2008f3187edf0d4cb705005
Dlani Mendes <dlanileonardo@gmail.com>
2014-07-15 04:06:54 -0300

    Ajustes

--- s3fs-prestashop/recipes/default.rb
+++ s3fs-prestashop/recipes/default.rb
@@ -4,15 +4,23 @@ node[:deploy].each do |application, deploy|
     next
   end
 
-  directory "/mnt/themes/bootstrap-theme/cache" do
-    owner deploy[:user]
-    group deploy[:group]
-    mode 0777
-    action :create
-    recursive: true
+  # directory "/mnt/themes/bootstrap-theme/cache" do
+  #   owner deploy[:user]
+  #   group deploy[:group]
+  #   mode 0777
+  #   action :create
+  #   recursive: true
+  # end
+
+  # Link cache do tema
+  link "#{deploy[:deploy_to]}/current/themes/bootstrap-theme/cache" do
+    to "/mnt/themes/bootstrap-theme/cache"
+    only_if do
+      File.exists?("/mnt/themes/bootstrap-theme/cache") && !File.exists?("#{deploy[:deploy_to]}/current/themes/bootstrap-theme/cache")
+    end
   end
 
-  Link img to S3
+  # Link img to S3
   link "#{deploy[:deploy_to]}/current/img" do
     to "/mnt/aws/img"
     only_if do
