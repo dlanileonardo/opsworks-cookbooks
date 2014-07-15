87f07bd8320437685c0eef4c86a8a37d210bb67c
Dlani Mendes <dlanileonardo@gmail.com>
2014-07-15 01:04:38 -0300

    Chmod

--- prestashop/recipes/default.rb
+++ prestashop/recipes/default.rb
@@ -23,6 +23,17 @@ node[:deploy].each do |application, deploy|
     to "#{deploy[:deploy_to]}/shared/config/settings.inc.php"
   end
 
+  directory "/mnt/aws/themes/bootstrap-theme/cache/" do
+    owner deploy[:user]
+    group deploy[:group]
+    mode 0777
+    action :create
+  end
+
+  link "#{deploy[:deploy_to]}/current/theme/bootstrap-theme/cache" do
+    to "/mnt/aws/themes/bootstrap-theme/cache/"
+  end
+
   # Link img to S3
   # link "#{deploy[:deploy_to]}/current/img" do
   #   to "/mnt/aws/img"
