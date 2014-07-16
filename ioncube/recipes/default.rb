55bd9c12d21e727ebc37f8805f161af48f3eea95
Dlani Mendes <dlanileonardo@gmail.com>
2014-07-16 07:15:17 -0300

    Ioncub

--- ioncube/recipes/default.rb
+++ ioncube/recipes/default.rb
@@ -5,7 +5,8 @@ bash "ioncube" do
   code <<-EOH
   (cd /tmp; wget http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_x86.tar.bz2)
   (cd /tmp; tar jxvf ioncube_loaders_lin_x86.tar.bz2)
-  (cd /tmp/ioncube; mv ioncube_loader_lin_5.5.so /usr/lib/php5/20121212/ioncube_loader_lin_5.5.so)
+  (cd /tmp/ioncube; mv ioncube_loader_lin_5.5.so /usr/lib/php5/20121212/ioncube_loader.so)
+  (chown root:root /usr/lib/php5/20121212/ioncube_loader_lin_5.5.so)
   EOH
 end
 
