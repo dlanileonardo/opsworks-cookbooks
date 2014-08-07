c63c2247f13a95761cf5d0c50bf106ab44f98d0a
Dlani Mendes <dlanileonardo@gmail.com>
2014-08-07 18:18:04 -0300

    Theme

--- prestashop/recipes/default.rb
+++ prestashop/recipes/default.rb
@@ -34,12 +34,16 @@ node[:deploy].each do |application, deploy|
     group deploy[:group]
   end
 
+  execute "mkdir -p /mnt/aws/themes/bootstrap-theme/"
+
   # link to aws/theme
   link "#{deploy[:deploy_to]}/current/themes/bootstrap-theme/cache" do
     to "/mnt/aws/themes/bootstrap-theme/cache"
     owner deploy[:user]
     group deploy[:group]
   end
+
+  execute "rm -rf /mnt/aws/themes/bootstrap-theme/"
 
   # assets
   execute "cp -R #{deploy[:deploy_to]}/current/themes/bootstrap-theme/assets/ /mnt/aws/themes/bootstrap-theme/" do
